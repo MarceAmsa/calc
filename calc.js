27fc52440cc88289ee645a35ad92721d00267865
Marcela Amador Salas <marce.amsa@gmail.com>
2017-01-16 12:18:26 +0100

    first commit

---
+++ calc.js
@@ -0,0 +1,22 @@
+(function myMainFunction(){
+  console.log("it's alive!");
+
+  var numbers = document.getElementsByClassName("numbers");
+
+  numbers.id.addEventListener('click', function (ev){
+    console.log("number button PRESSED ");
+    console.log("the target ID is..." + ev.target.id);
+
+  })
+
+})();
+
+// NOTES
+//
+// x = first number pressed,
+// y = second number pressed
+//
+// x+y = x
+// if suma is activated
+//
+// x + second number pressed = y
