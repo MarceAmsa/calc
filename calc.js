56131beba11230f667b4cda280629ba2197c3307
Marcela Amador Salas <marce.amsa@gmail.com>
2017-01-17 11:28:23 +0100

    changes

--- calc.js
+++ calc.js
@@ -24,26 +24,51 @@
   function updateValue(val_1){
       // Store value
       value += val_1;
-
       // Display update
-      displayShow.innerHTML += value;
-
-      addOp.addEventListener('click', function(event){
-        console.log("plus");
-        var newValue = '';
-
-        function updateValue(val_2) {
-          newValue += val_2;
-
-          displayShow.innerHTML += parseFloat (newValue) + parseFloat(value);
-        }
-
-      })
-
+      displayShow.innerHTML += val_1;
       // value = parseFloat (value) + parseFloat (value_2);
   }
 
 
+var clear = false;
+  addOp.addEventListener ('click', function (ev) {
+    clear = true;
+    console.log(clear);
+  });
+
+var reset = false;
+  for (var i = 0; i < myNumber.length; i++) {
+    myNumber[i].addEventListener('click', function (event){
+      if (clear == true) {
+        clear == false;
+        displayShow.innerHTML = val_2; //NO SIRVE
+      }
+  });
+}
+
+
+var reset = false;
+  for (var i = 0; i < myNumber.length; i++) {
+    myNumber[i].addEventListener('click', function (event){
+      if (clear == true) {
+        clear == false;
+        displayShow.innerHTML = val_2; //NO SIRVE
+
+        var newValue = '';
+        function updateValue(val_2) {
+          newValue += val_2;
+          displayShow.innerHTML += parseFloat (newValue) + parseFloat(value);
+        }
+      }
+  });
+}
+// function clickNumero() {
+// if(reset == true) {
+// limpiarDisplay;
+// reset = false;
+// }
+// agregarValorAlDisplay();
+// }
 
 
 
