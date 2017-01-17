df527acaaace636989c3ee33684a183f745d8f23
Marcela Amador Salas <marce.amsa@gmail.com>
2017-01-17 11:47:57 +0100

    before changes in var reset

--- calc.js
+++ calc.js
@@ -41,39 +41,19 @@ var reset = false;
     myNumber[i].addEventListener('click', function (event){
       if (clear == true) {
         clear == false;
-        displayShow.innerHTML = val_2; //NO SIRVE
-      }
-  });
-}
 
-
-var reset = false;
-  for (var i = 0; i < myNumber.length; i++) {
-    myNumber[i].addEventListener('click', function (event){
-      if (clear == true) {
-        clear == false;
-        displayShow.innerHTML = val_2; //NO SIRVE
-
-        var newValue = '';
-        function updateValue(val_2) {
-          newValue += val_2;
-          displayShow.innerHTML += parseFloat (newValue) + parseFloat(value);
+        reset == true;
+        value = '';
+        function updateValue(val_1){
+            // Store value
+            value += val_1;
+            // Display update
+            displayShow.innerHTML += val_1;
+            // value = parseFloat (value) + parseFloat (value_2);
         }
       }
   });
 }
-// function clickNumero() {
-// if(reset == true) {
-// limpiarDisplay;
-// reset = false;
-// }
-// agregarValorAlDisplay();
-// }
-
-
-
-
-
 
 
 })();
