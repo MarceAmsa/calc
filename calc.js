7cbf3576e16d0257d0a6507f2e5e76e376450201
Marcela Amador Salas <marce.amsa@gmail.com>
2017-01-18 11:37:13 +0100

    new changes from webstorm. undefined error when + is pressed

--- calc.js
+++ calc.js
@@ -1,69 +1,78 @@
-(function myMainFunction(){
-  console.log("it's alive!");
+(function myMainFunction() {
+    console.log("it's alive!");
 
 
-  var myNumber = document.getElementsByClassName("numbers");
-  var displayShow = document.getElementById('display');
-  var addOp = document.getElementById('add');
-  var acButton = document.getElementById('ac');
+    var myNumber = document.getElementsByClassName("numbers");
+    var displayShow = document.getElementById('display');
+    var addOp = document.getElementById('add');
+    var acButton = document.getElementById('ac');
+    var equalsButton = document.getElementById('equals');
 
-  var myFunction = function () {
-    var attribute = this.getAttribute("data-value");
-    alert(attribute);
-  };
-
-  for (var i = 0; i < myNumber.length; i++) {
-      myNumber[i].addEventListener('click', function (event){
-        console.log("you pressed " + this.id);
-
-
-        updateValue( this.getAttribute("data-value") );
-      });
-  }
-
-  var value = '';
-  function updateValue(val_1){
-      // Store value
-      value += val_1;
-      // Display update
-      displayShow.innerHTML += val_1;
-      // value = parseFloat (value) + parseFloat (value_2);
-  }
-
-
-var clear = false;
-  addOp.addEventListener ('click', function (ev) {
-    clear = true;
-    console.log(clear);
-    displayShow.innerHTML = value;
+    var myFunction = function () {
+        var attribute = this.getAttribute("data-value");
+        alert(attribute);
+    };
 
     for (var i = 0; i < myNumber.length; i++) {
+        myNumber[i].addEventListener('click', function (event) {
+            console.log("you pressed " + this.id);
 
-        myNumber[i].addEventListener('click', function (event){
-          if (clear == true) {
-          clear = false;
-          console.log(clear);
-          displayShow.innerHTML = " ";
-          console.log("reset done");
-        }
-      });
+
+            updateValue(this.getAttribute("data-value"));
+        });
     }
-});
+
+    var value = '';
+
+    function updateValue(val_1) {
+        // Store value
+        value += val_1;
+        // Display update
+        displayShow.innerHTML += val_1;
+        // value = parseFloat (value) + parseFloat (value_2);
+    }
+
+//CLEAR DISPLAY
+
+    var clear = false;
+    addOp.addEventListener('click', function (ev) {
+        clear = true;
+        console.log(clear);
+        displayShow.innerHTML = myNumber[i];
+
+        for (var i = 0; i < myNumber.length; i++) {
+            myNumber[i].addEventListener('click', function (event) {
+                if (clear == true) {
+                    clear = false;
+                    console.log(clear);
+                    console.log("reset done");
+                    //displayShow.innerHTML = myNumber[i];
+
+                    //algo acerca de value_2
+
+                }
+            });
+        }
+    });
 
 
-
+// RESULT BUTTON ------------------
+    equalsButton.addEventListener('click', function (ev) {
+        console.log("result must be shown");
+        // value = parseFloat(value) + parseFloat(value_2);
+        displayShow.innerHTML = parseFloat(value);
+    })
 
 
 // AC RESET BUTTON ------------------
-  acButton.addEventListener('click', function (ev){
-    console.log("resetPressed");
-    value = '';
-    displayShow.innerHTML = ' ';
-  })
+    acButton.addEventListener('click', function (ev) {
+        console.log("resetPressed");
+        value = '';
+        displayShow.innerHTML = ' ';
+    })
 
 
 })();
-
 
 
 // Operators for functions
