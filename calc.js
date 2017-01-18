e4ff957a78ae5a7ce1a8773d8b04c60f34e88b00
Marcela Amador Salas <marce.amsa@gmail.com>
2017-01-18 17:16:07 +0100

    comments everywhere

--- calc.js
+++ calc.js
@@ -17,62 +17,115 @@
         myNumber[i].addEventListener('click', function (event) {
             console.log("you pressed " + this.id);
 
-
-            updateValue(this.getAttribute("data-value"));
+            updateValue(this.getAttribute("data-value")); // conectado con la variable update value de abajo
         });
     }
 
-    var value = '';
-    var x = '';
-    var y = '';
     var result = '';
 
-    function updateValue(val_1) {
-        // Store value
-        value += val_1;
+    // Save user's input
+    inputArray = [];
+
+    function updateValue(val_1) { // acá jala los atributos de data value que habían arriba
         // Display update
         displayShow.innerHTML += val_1;
-
-        // value = parseFloat (value) + parseFloat (value_2);
+        inputArray.push(val_1) //suma los inputs que se realizar a inputArray
     }
 
 //CLEAR DISPLAY
 
     var clear = false;
     addOp.addEventListener('click', function (ev) {
-        clear = true;
-        console.log(clear);
-        x = value;
-        displayShow.innerHTML = myNumber[i];
+
+        inputArray.push('+');
 
 
-        for (var i = 0; i < myNumber.length; i++) { //UNDEFINED ERROR HERE although it works and clears out value
-            myNumber[i].addEventListener('click', function (event) {
-                if (clear == true) {
+        displayShow.innerHTML = '';
 
 
-                    clear = false;
-
-                    y = value;
-                    console.log(clear);
-                    console.log("reset done");
-                    //displayShow.innerHTML = myNumber[i];
-
-                    //algo acerca de value_2
-
-                }
-            });
-        }
     });
 
 
 // RESULT BUTTON ------------------
 
+    function inputParser(){
+        var numberAndOperatorArray = []; // se nombra las variables no hace nada
+
+        var currentNumber = ''; // nse nombra la variable, tampoco hace nada
+
+        // Loop through input: ['1', '.', '1', '+', '1']
+        for( var i=0 ; i < inputArray.length ; i++ ){ // para cada elemento del array de 0 a lo que sea (inputArray.length) es lo máx
+
+            // Check if current input is a number
+            if( ! isNaN( parseInt(inputArray[i]) ) ){  //isNaN es "no es número" pero con ! es lo contrario ósea "es número"
+                // Concatenate number
+                currentNumber += inputArray[i]; //inputArray es una lista diferente a numberAndOperatorArray
+                //la línea alimenta todos los currentnumber a input array que está arriba como variable definida
+
+            } else {
+                // Current input is not a number ( = it is a . or operator
+                switch(inputArray[i]){
+                    case '.':
+                        currentNumber += '.';
+                    break;
+                    // Default handle all of the operator
+                    default:
+                        // Save number into value array
+                        numberAndOperatorArray.push(parseFloat(currentNumber));
+
+                        // Save operator into value array
+                        numberAndOperatorArray.push(inputArray[i]);
+
+                        // Reset current number
+                        currentNumber = ''
+
+                }
+            }
+        }
+
+        // Save last number into the value array
+        numberAndOperatorArray.push(parseFloat(currentNumber));
+
+
+        // Contain the calculation result - Save first number in
+        var result = numberAndOperatorArray[0];
+
+        // Loop through number and operators
+        for( var j=1 ; j < numberAndOperatorArray.length ; j++ ){
+
+            // If not a number -> Do operation against the next number (j+1)
+            if( isNaN(numberAndOperatorArray[j])){
+                // Check which operator this is
+                switch(numberAndOperatorArray[j]){
+                    case '+':
+                        result += numberAndOperatorArray[j+1];
+                        break;
+                    case '-':
+                        result -= numberAndOperatorArray[j+1];
+                    break;
+                }
+            }
+        }
+
+        console.log(result)
+
+
+
+
+    }
+
     equalsButton.addEventListener('click', function (ev) {
-        console.log("result must be shown");
-        // value = parseFloat(value) + parseFloat(value_2);
-        result = parseFloat(x);
-        displayShow.innerHTML = result;
+
+        inputParser();
+
+        // if (clear == true){
+        //     y = value ; // y = value -x; as a string
+        //     clear = false;
+        // }
+        // console.log("result must be shown");
+        // // value = parseFloat(value) + parseFloat(value_2);
+        // result = parseFloat(x) + parseFloat(value);
+        // displayShow.innerHTML = result;
 
     })
 
