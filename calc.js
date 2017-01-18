4d8a1e2e8254a0fbd44fee9718dab08440784df7
Marcela Amador Salas <marce.amsa@gmail.com>
2017-01-18 19:24:51 +0100

    understand

--- calc.js
+++ calc.js
@@ -48,7 +48,7 @@
     function inputParser(){
         var numberAndOperatorArray = []; // se nombra las variables no hace nada
 
-        var currentNumber = ''; // nse nombra la variable, tampoco hace nada
+        var stringNumber = ''; // se nombra la variable, tampoco hace nada
 
         // Loop through input: ['1', '.', '1', '+', '1']
         for( var i=0 ; i < inputArray.length ; i++ ){ // viene de la función update value, va uno por uno
@@ -57,32 +57,32 @@
             if( ! isNaN( parseInt(inputArray[i]) ) ){  //isNaN es "no es número" pero con ! es lo contrario ósea "es número"
                 // Concatenate number
                 // input array son todos los botones que se presionaron anteriormente lo que genera una lista, que luego se concatena
-                currentNumber += inputArray[i]; //inputArray es una lista diferente a numberAndOperatorArray
+                stringNumber += inputArray[i]; //inputArray es una lista diferente a numberAndOperatorArray
                 //la línea alimenta todos los currentnumber a input array que está arriba como variable definida
 
             } else {
                 // Current input is not a number ( = it is a . or operator
                 switch(inputArray[i]){
                     case '.':
-                        currentNumber += '.'; // si en esa lista hay un punto, entonces el punto se mete dentro del número
+                        stringNumber += '.'; // si en esa lista hay un punto, entonces el punto se mete dentro del número
                     break;
                     // Default handle all of the operator
                     default:
                         // Save number into value array
-                        numberAndOperatorArray.push(parseFloat(currentNumber)); //ACÁ es que se empieza a crear el numberOperatorArray, antes de eso estaba solamente vacía
+                      numberAndOperatorArray.push(parseFloat(stringNumber)); //ACÁ es que se empieza a crear el numberOperatorArray, antes de eso estaba solamente vacía
 
                         // Save operator into value array
-                        numberAndOperatorArray.push(inputArray[i]);
+                       numberAndOperatorArray.push(inputArray[i]);
 
                         // Reset current number
-                        currentNumber = ''
+                        stringNumber = ''
 
                 }
             }
         }
 
         // Save last number into the value array
-        numberAndOperatorArray.push(parseFloat(currentNumber));
+        numberAndOperatorArray.push(parseFloat(stringNumber));
 
 
         // Contain the calculation result - Save first number in
@@ -112,9 +112,10 @@
 
     }
 
+    // RESULT ------------------
     equalsButton.addEventListener('click', function (ev) {
 
-        inputParser();
+        inputParser(); // efectuar la función
 
         // if (clear == true){
         //     y = value ; // y = value -x; as a string
@@ -141,3 +142,48 @@
 
 
 // Operators for functions
+
+
+Tengo numberAnd [] y inputArray [i]
+
+Presiono tecla 3 el proceso es:
+
+3 ! NaN es true
+
+entonces inputArray ["3"]
+stringNumber = "3"
+
+Presiono tecla 5 el proces es:
+
+5 ! NaN es true
+
+entonces inputArray es ["3", "5"];
+stringNumber es "35"
+
+Presiono "+"
+
+Ahora + ! NaN false
+entonces
+numberAnd [parseFloat de "35"]
+lo hace numberAnd [35]
+
+y luego
+numberAnd [35, "+"]
+stringNumber = ''
+
+esto sólo genera el string nada más
+cuando se tiene por ejemplo numberAnd [33, 'x', 55]
+se presiona '='
+
+dice que ahora el numberAnd [0] es asignado en result (nada cambia en el array sólo se asigna para que el registro del array vaya de 0... en adelante)
+
+
+luego se va a través del array con j++. De ahí sigue hasta que se topa con un NaN
+
+dependiendo del caso hace cosas diferentes pero en el caso de la suma es
+result += numberAndOperatorArray[j+1];
+lo que quiere decir  que es el número parsed de "StringNumber" más el que le sigue a "+" (siendo + en ese momento numberAnd[j] por ende
+numberAnd[j+1] es el que le sigue en la lista)
+
+
+
