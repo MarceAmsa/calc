bf7fa8412a0b8f16e2c94995d3fedf97d1869023
Marcela Amador Salas <marce.amsa@gmail.com>
2017-01-24 15:06:18 +0100

    pressing = not longer generates and undefines

--- calc.js
+++ calc.js
@@ -12,7 +12,13 @@
         myNumber[i].addEventListener('click', function (event) {
             console.log("you pressed " + this.id);
             acButton.innerHTML = 'C';
-            updateValue(this.getAttribute("data-value")); // Toma data-value del html
+
+
+            updateValue(this.getAttribute("data-value"), resultGiven); // Toma data-value del html
+
+            if (resultGiven) {
+                resultGiven = false;
+            }
 
 
         });
@@ -20,45 +26,44 @@
 
     // Save user's input
     inputArray = [];
-    function updateValue(val_1) { // update de los números presionados
+    function updateValue(val_1, reset) { // update de los números presionados
 
-        if (resultGiven == true) {
-            inputArray = [];
-            resultGiven = false;
+        // if (reset) {
+        //     inputArray = [];
+        //     displayShow.innerHTML = val_1;
+        //
+        // } else {
+        //     displayShow.innerHTML += val_1;
+        // }
 
-        }
 
         // Display update
         displayShow.innerHTML += val_1;
         inputArray.push(val_1) //los agrega al array
 
-
-
-
-
-     }
+    }
 
 
 // ----------------------
 
-    for( var i = 0 ; i < myOperator.length ; i++ ){
+    for (var i = 0; i < myOperator.length; i++) {
         myOperator[i].addEventListener('click', function (ev) {
-                console.log("operator pressed " + this.id);
-                inputArray.push(this.getAttribute("data-operator"));
-                displayShow.innerHTML = '';
-            });
+            console.log("operator pressed " + this.id);
+            inputArray.push(this.getAttribute("data-operator"));
+            displayShow.innerHTML = '';
+        });
     }
 
-    function inputParser(){
+    function inputParser() {
         var numberAndOperatorArray = [];
 
         var stringNumber = ''; // Número actual en string
 
         // Loop through input: ['1', '.', '1', '+', '1']
-        for( var i=0 ; i < inputArray.length ; i++ ){
+        for (var i = 0; i < inputArray.length; i++) {
 
             // Check if current input is a number
-            if( ! isNaN( parseInt(inputArray[i]) ) ){  // ! es un double negative
+            if (!isNaN(parseInt(inputArray[i]))) {  // ! es un double negative
                 // Concatenate number
 
                 stringNumber += inputArray[i];
@@ -67,17 +72,17 @@
 
             } else {
                 // Current input is not a number ( = it is a . or operator
-                switch(inputArray[i]){
+                switch (inputArray[i]) {
                     case '.':
                         stringNumber += '.'; // Agrega '.' al inputArray
-                    break;
+                        break;
                     // Default handle all of the operator
                     default:
                         // Save number into value array
-                      numberAndOperatorArray.push(parseFloat(stringNumber)); // Se llena NumberAndOperatorArray
+                        numberAndOperatorArray.push(parseFloat(stringNumber)); // Se llena NumberAndOperatorArray
 
                         // Save operator into value array
-                       numberAndOperatorArray.push(inputArray[i]);
+                        numberAndOperatorArray.push(inputArray[i]);
 
                         // Reset current number
                         stringNumber = '';
@@ -92,7 +97,7 @@
         // Save last number into the value array
         numberAndOperatorArray.push(parseFloat(stringNumber));
 
-        for (var i = 0; i < numberAndOperatorArray.length ; i ++ ) {
+        for (var i = 0; i < numberAndOperatorArray.length; i++) {
             if (isNaN(numberAndOperatorArray[i])) {
                 // Check which operator this is
 
@@ -103,7 +108,7 @@
                         mult = (numberAndOperatorArray [i - 1]) * (numberAndOperatorArray[i + 1]);
                         numberAndOperatorArray.splice(i - 1, 3, mult);
 
-                        if (numberAndOperatorArray.length == 1) {
+                        if (numberAndOperatorArray.length <= 1) {
                             result = mult;
                         }
                         break;
@@ -111,7 +116,7 @@
                     case '÷':
                         division = (numberAndOperatorArray [i - 1]) / (numberAndOperatorArray[i + 1]);
                         numberAndOperatorArray.splice(i - 1, 3, division);
-                        if (numberAndOperatorArray.length == 1) {
+                        if (numberAndOperatorArray.length <= 1) {
                             result = division;
                         }
                         break;
@@ -120,19 +125,20 @@
         }
 
 // ADD AND SUBS INTO STRING
-        for( var j =0 ; j < numberAndOperatorArray.length ; j++ ){
+        for (var j = 0; j < numberAndOperatorArray.length; j++) {
 
             // If not a number -> Do operation against the next number (j+1)
-            if( isNaN(numberAndOperatorArray[j])){
+            if (isNaN(numberAndOperatorArray[j])) {
                 // Check which operator this is
-                switch(numberAndOperatorArray[j]){
+                switch (numberAndOperatorArray[j]) {
                     case '+':
-                        result += (numberAndOperatorArray [j+1]);
+                        result += (numberAndOperatorArray [j + 1]);
                         break;
 
                     case '-':
-                        result -= (numberAndOperatorArray [j+1]);
+                        result -= (numberAndOperatorArray [j + 1]);
                         break;
+
                 }
             }
         }
@@ -145,10 +151,20 @@
     // RESULT ------------------
     resultGiven = false;
     equalsButton.addEventListener('click', function (ev) {
-        inputParser(); // efectuar la función
 
+        if ((inputArray.length == 0) || (resultGiven == true)){
+            return
+        }
+
+        if (inputArray.length == 1 ) {
+            return inputArray[i];
+        }
+
+        inputParser(); // efectuar la función
         resultGiven = true;
         console.log(resultGiven);
+
+
 
 
     })
@@ -157,23 +173,23 @@
     var resetPressed = false;
 
     // AC RESET BUTTON ------------------
-        acButton.addEventListener('click', function (ev) {
+    acButton.addEventListener('click', function (ev) {
 
-            if (resetPressed == false) {
+        if (resetPressed == false) {
             resetPressed = true;
             value = '';
             inputArray = [];
 
-            } else {
+        } else {
 
             value = '';
             ev.target.innerHTML = 'AC';
             displayShow.innerHTML = ' ';
             inputArray = [];
-          //  resetPressed = false;
+            //  resetPressed = false;
 
-            }
-        })
+        }
+    })
 
 })();
 
